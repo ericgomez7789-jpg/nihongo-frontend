711218036a345348833cb3acbfbe756acc90aa88
Eric Gomez <ericgomez7789@gmail.com>
2026-07-21 08:06:38 -0700

    Added file to correct supabase behavior

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://dpaugaohbgpbtsstfihm.supabase.co",
+  "sb_publishable_RQV6i4UiMHXTEOs1L0xpYQ_ug4IRSXr"
+);
